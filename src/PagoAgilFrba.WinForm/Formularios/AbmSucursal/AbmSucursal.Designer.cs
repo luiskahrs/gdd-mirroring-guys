60df02b072bcf26f115fb0b368b7700523c27168
Luis Kahrs <luisk@lagash.com>
2017-11-12 08:13:22 -0300

    Sucursales

--- src/PagoAgilFrba.WinForm/Formularios/AbmSucursal/AbmSucursal.Designer.cs
+++ src/PagoAgilFrba.WinForm/Formularios/AbmSucursal/AbmSucursal.Designer.cs
@@ -28,19 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.txtDni = new System.Windows.Forms.TextBox();
+            this.lblDni = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.grpBusqueda.SuspendLayout();
             this.SuspendLayout();
             // 
-            // AbmSucursal
+            // grpBusqueda
+            // 
+            this.grpBusqueda.Controls.Add(this.txtDni);
+            this.grpBusqueda.Controls.Add(this.lblDni);
+            this.grpBusqueda.Controls.Add(this.txtApellido);
+            this.grpBusqueda.Controls.Add(this.lblApellido);
+            this.grpBusqueda.Controls.Add(this.txtNombre);
+            this.grpBusqueda.Controls.Add(this.lblNombre);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(69, 19);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(291, 20);
+            this.txtNombre.TabIndex = 4;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(15, 22);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(44, 13);
+            this.lblNombre.TabIndex = 3;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(69, 45);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(291, 20);
+            this.txtApellido.TabIndex = 6;
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Location = new System.Drawing.Point(15, 48);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(44, 13);
+            this.lblApellido.TabIndex = 5;
+            this.lblApellido.Text = "Apellido";
+            // 
+            // txtDni
+            // 
+            this.txtDni.Location = new System.Drawing.Point(69, 71);
+            this.txtDni.MaxLength = 8;
+            this.txtDni.Name = "txtDni";
+            this.txtDni.Size = new System.Drawing.Size(291, 20);
+            this.txtDni.TabIndex = 8;
+            // 
+            // lblDni
+            // 
+            this.lblDni.AutoSize = true;
+            this.lblDni.Location = new System.Drawing.Point(15, 74);
+            this.lblDni.Name = "lblDni";
+            this.lblDni.Size = new System.Drawing.Size(26, 13);
+            this.lblDni.TabIndex = 7;
+            this.lblDni.Text = "DNI";
+            // 
+            // AbmCliente
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Name = "AbmSucursal";
-            this.Text = "Abm de Sucursal";
+            this.ClientSize = new System.Drawing.Size(590, 511);
+            this.Name = "AbmCliente";
+            this.Text = "ABM Clientes";
+            this.panel1.ResumeLayout(false);
+            this.grpBusqueda.ResumeLayout(false);
+            this.grpBusqueda.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.TextBox txtDni;
+        private System.Windows.Forms.Label lblDni;
     }
 }
--- src/PagoAgilFrba.WinForm/Formularios/AbmSucursal/AbmSucursal.cs
+++ src/PagoAgilFrba.WinForm/Formularios/AbmSucursal/AbmSucursal.cs
@@ -1,20 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace PagoAgilFrba
+﻿namespace PagoAgilFrba
 {
-    public partial class AbmSucursal : Form
+    using Core;
+    using System;
+    using System.Data;
+    using System.Windows.Forms;
+
+    public partial class AbmSucursal : FormListaBase
     {
         public AbmSucursal()
         {
             InitializeComponent();
         }
+        protected override void InicializarFormulario()
+        {
+            base.InicializarFormulario();
+        }
+        protected override DataTable RecuperarDatos()
+        {
+            decimal dni;
+
+            if (string.IsNullOrEmpty(txtDni.Text))
+            {
+                dni = 0;
+            }
+            else if (!decimal.TryParse(txtDni.Text, out dni))
+            {
+                throw new PagoAgilException("El DNI debe ser un valor numerico.");
+            }
+            else
+            {
+            }
+
+            return Cliente.Listar(txtNombre.Text, txtApellido.Text, dni);
+        }
+
+        protected override void AbrirElemento(DataGridViewRow dr)
+        {
+            var cliente = CrearClienteDesdeDataRow(dr);
+            var editor = new EditorCliente(cliente);
+
+            if (editor.ShowDialog() == DialogResult.OK)
+            {
+                CargarGrilla();
+            }
+        }
+
+        protected override void EliminarElemento(DataGridViewRow dr)
+        {
+            if (MessageBox.Show("Esta seguro que desea eliminar el cliente?", "Eliminar cliente", 
+                    MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                Cliente cliente = CrearClienteDesdeDataRow(dr);
+                cliente.Eliminar();
+                this.CargarGrilla();
+            }
+        }
+
+        public override bool AgregarElemento()
+        {
+            EditorCliente editor = new EditorCliente(new Cliente());
+            return editor.ShowDialog() == DialogResult.OK;
+        }
+
+        public Cliente CrearClienteDesdeDataRow(DataGridViewRow dr)
+        {
+            Cliente cliente = Cliente.Obtener(Convert.ToInt32(dr.Cells["Id"].Value));
+            return cliente;
+        }     
     }
 }
